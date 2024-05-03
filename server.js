da02e05531d40ca6416e8d1339c6e59d04f084f4
David Petrut <dreaigan_best_david@yahoo.com>
2024-05-03 23:52:01 +0300

    all done

--- server.js
+++ server.js
@@ -5,32 +5,33 @@ const app = express();
 // Define the path to the public directory
 const publicPath = path.join(__dirname, "public");
 
-// Serve static files from the 'public' directory
-app.use(express.static(publicPath));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
 
 // Main routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(publicPath, "about.html"));
+  res.sendFile(path.join(__dirname, "about.html"));
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(publicPath, "contact.html"));
+  res.sendFile(path.join(__dirname, "contact.html"));
 });
 
-// Custom 404 page
+// Update the paths for the error pages as well
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(publicPath, "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
 });
 
-// Custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).sendFile(path.join(publicPath, "503.html"));
+  res.status(500).sendFile(path.join(__dirname, "503.html"));
 });
+
 
 // Set the port and start the server
 const PORT = process.env.PORT || 3000;
